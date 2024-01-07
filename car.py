460ce4e9945a14c04e300323af15f8c39b808651
Muhammad <muhammadalnabhani@gmail.com>
2024-01-07 21:53:59 +0000

    create tire ware class & update spindler battery

--- car.py
+++ car.py
@@ -1,11 +1,13 @@
 from serviceable import Serviceable
 from engine import Engine
 from battery import Battery
+from tire_ware import Tire_Ware
 
 class Car(Serviceable):
-    def __init__(self, engine:Engine, battery:Battery):
+    def __init__(self, engine:Engine, battery:Battery, tire_ware:Tire_Ware):
         self.engine = engine
         self.battery = battery
+        self.tire_ware = tire_ware
 
     def needs_service(self):
-         return self.engine.needs_service() or self.battery.needs_service()
+         return self.engine.needs_service() or self.battery.needs_service() or self.tire_ware.tire_needs_service()
---
+++ tire_ware.py
@@ -0,0 +1,33 @@
+from abc import ABC
+
+
+class Tire_Ware(ABC):
+    def __init__(self,tire_ware_sensor) -> None:
+        self.tire_ware_sensor = tire_ware_sensor
+
+    def tire_needs_service(self):
+        pass
+
+class Carigan_Tire(Tire_Ware):
+    def __init__(self, tire_ware_sensor) -> None:
+        super().__init__(tire_ware_sensor)
+    
+    def tire_needs_service(self):
+        ware_count = 0
+        for value in self.tire_ware_sensor:
+            if value >= 0.9:
+                ware_count+=1
+
+        return ware_count>=1
+    
+class Octoprime_tire(Tire_Ware):
+    def __init__(self, tire_ware_sensor) -> None:
+        super().__init__(tire_ware_sensor)
+    
+    def tire_needs_service(self):
+        sum = 0
+        for value in self.tire_ware_sensor:
+            sum+=value
+                
+        return sum>=3
+            

c15d49940056df1ef23abe0a0179fe0d5bd7a257
Muhammad <muhammadalnabhani@gmail.com>
2024-01-07 11:26:14 +0000

    add remaining car type methods

--- car_factory.py
+++ car_factory.py
@@ -1,6 +1,6 @@
 from datetime import date
-from engine import CapuletEngine
-from battery import SpinderBattery
+from engine import CapuletEngine, WilloughbyEngine, SternmanEngine
+from battery import SpinderBattery, NubbinBattery
 from car import Car
 class CarFactory:
     def create_calliope(self,current_date,last_service_date,current_mileage,last_service_mileage):
@@ -14,3 +14,47 @@ class CarFactory:
 
         return Car(engine,battery) 
     
+    def create_glissade(self,current_date,last_service_date,current_mileage,last_service_mileage):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        self.current_mileage = current_mileage
+        self.last_service_date = last_service_date
+
+        engine = WilloughbyEngine(last_service_mileage,current_mileage)
+        battery = SpinderBattery(last_service_mileage,current_mileage)
+
+        return Car(engine,battery)
+    
+
+    def create_palindrome(self,current_date,last_service_date, warning_light_on):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        self.warning_light_on = warning_light_on
+
+        engine = SternmanEngine(warning_light_on)
+        battery = SpinderBattery(last_service_date,current_date)
+
+        return Car(engine,battery)
+
+    
+    def create_rorschach(self,current_date,last_service_date,current_mileage,last_service_mileage):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        self.current_mileage = current_mileage
+        self.last_service_date = last_service_date
+
+        engine = WilloughbyEngine(last_service_mileage,current_mileage)
+        battery = NubbinBattery(last_service_date,current_date)
+
+        return Car(engine,battery)
+    
+    def create_rorschach(self,current_date,last_service_date,current_mileage,last_service_mileage):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        self.current_mileage = current_mileage
+        self.last_service_date = last_service_date
+
+        engine = CapuletEngine(last_service_mileage,current_mileage)
+        battery = NubbinBattery(last_service_date,current_date)
+
+        return Car(engine,battery)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import date
 from car_factory import CarFactory
-
+from engine import CapuletEngine
 
 car1 = CarFactory()
 calliope = car1.create_calliope(date(2023, 12, 30), date(2021, 4, 16), 90098, 68201)
-
+calliope.engine = CapuletEngine(1200,1300)
 print(car1)
-print(calliope)
+print(calliope.engine.needs_service())
